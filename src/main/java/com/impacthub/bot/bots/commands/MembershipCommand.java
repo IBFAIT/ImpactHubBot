d86a8782725c8b1a9769d94c56b9c1b0b50bef8d
IBFAIT <stuff@soundz.ch>
2020-03-05 10:52:43 +0100

    Bug fixed Refactoring

--- src/main/java/com/impacthub/bot/bots/commands/MembershipCommand.java
+++ src/main/java/com/impacthub/bot/bots/commands/MembershipCommand.java
@@ -1,8 +1,5 @@
 package com.impacthub.bot.bots.commands;
 
-import com.impacthub.bot.Application;
-import com.impacthub.bot.bots.Concierge;
-import com.impacthub.bot.services.Constants;
 import com.impacthub.bot.services.authorisation.AuthorisationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,12 +10,18 @@ import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.Date;
+
+import static com.impacthub.bot.services.Constants.*;
+
 public class MembershipCommand extends BotCommand {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MembershipCommand.class);
+    private final AuthorisationService authService;
 
-    public MembershipCommand() {
+    public MembershipCommand(AuthorisationService authService) {
         super("membership", "Get Membership Status");
+        this.authService = authService;
     }
 
     @Override
@@ -30,27 +33,32 @@ public class MembershipCommand extends BotCommand {
             userName = user.getFirstName();
         }
 
-        String phoneNumber = Concierge.getPhoneNumber(user.getId());
+
+        String phoneNumber = authService.getPhoneNumberFromUserId(user.getId());
         SendMessage message = new SendMessage();
         message.setChatId(chat.getId());
 
         try {
             if (phoneNumber != null) {
-                AuthorisationService authService = (AuthorisationService) Application.getBean("authorisationService");
                 String membership = authService.getMembership(phoneNumber);
 
-                if (!membership.equals(Constants.DEFAULT_MEMBERSHIP)) {
-                    message.setText("Your membership is [" + membership + "].");
+                if (!membership.equals(DEFAULT_MEMBERSHIP)) {
+
+                    //todo!
+                    Date expirationDate = authService.getMembershipExpirationDate(phoneNumber);
+                    int days = 243;
+                    message.setText("Your membership is '" + membership + "' and will expire in " + days + " days.");
                 } else {
-                    message.setText(Constants.NO_MEMBERSHIP);
+                    message.setText(NO_MEMBERSHIP);
                 }
-            } else
-                message.setText(String.format(Constants.UNAUTHORIZED_USER, this.getCommandIdentifier()));
+            } else {
+                message.setText(String.format(UNAUTHORIZED_USER, this.getCommandIdentifier()));
+            }
 
             absSender.execute(message);
 
-
         } catch (TelegramApiException e) {
+            LOGGER.error("Error executing membership command", e);
             e.printStackTrace();
         }
     }
